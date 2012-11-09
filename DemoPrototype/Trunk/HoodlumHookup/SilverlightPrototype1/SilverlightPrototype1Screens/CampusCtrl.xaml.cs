167ae9c6e8887b53f6f81b3f99d498525af7bb8b
Rand Williams <j dt rand dt williams at gm41l dt c0m>
2012-11-08 23:14:53 -0500

    Fall 2012 SPSU MS SWE Capstone. Brad, Rand, Jason, Zoltan. Initial commit of Silverlight Prototype of "Hoodlum Hookup"

---
+++ DemoPrototype/Trunk/HoodlumHookup/SilverlightPrototype1/SilverlightPrototype1Screens/CampusCtrl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightPrototype1Screens
+{
+	public partial class Campus : UserControl
+	{
+		public int? Which 
+  		{ 
+    		get { return base.GetValue(SourceProperty) as int?; } 
+    		set { base.SetValue(SourceProperty, value); } 
+  		}
+		
+  		public static readonly DependencyProperty SourceProperty = 
+    		DependencyProperty.Register("Which", typeof(int?), typeof(Campus), null); 
+
+		public Campus()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+		private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
---
+++ DemoPrototype/Trunk/HoodlumHookup/SilverlightPrototype1/SilverlightPrototype1Screens/MapCtrl.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+using Microsoft.Maps.MapControl;
+using Microsoft.Maps.MapControl.Design;
+
+namespace SilverlightPrototype1Screens
+{
+	public partial class MapCtrl : UserControl
+	{
+
+		
+		
+		public double? zoomLevel 
+  		{ 
+    		get { return base.GetValue(zoomLevel_Property) as double?; } 
+    		set { base.SetValue(zoomLevel_Property, value); Map_SetView(); } 
+  		}
+		
+  		public static readonly DependencyProperty zoomLevel_Property = 
+    		DependencyProperty.Register("zoomLevel", typeof(double?), typeof(MapCtrl),
+			new PropertyMetadata(new PropertyChangedCallback(OnChanged_zoomLevel_Property))); 
+		
+		private static void OnChanged_zoomLevel_Property(
+			DependencyObject d, DependencyPropertyChangedEventArgs e) 
+		{
+			//((MapCtrl)d).Map_SetView();
+		}
+		
+		
+		
+		public double? latitude 
+  		{ 
+    		get { return base.GetValue(latitude_Property) as double?; } 
+    		set { base.SetValue(latitude_Property, value); Map_SetView(); } 
+  		}
+		
+  		public static readonly DependencyProperty latitude_Property = 
+    		DependencyProperty.Register("latitude", typeof(double?), typeof(MapCtrl),
+			new PropertyMetadata(new PropertyChangedCallback(OnChanged_latitude_Property))); 
+		
+		private static void OnChanged_latitude_Property(
+			DependencyObject d, DependencyPropertyChangedEventArgs e) 
+		{
+			//((MapCtrl)d).Map_SetView();
+		}
+		
+		
+		
+		public double? longitude 
+  		{ 
+    		get { return base.GetValue(longitude_Property) as double?; } 
+    		set { base.SetValue(longitude_Property, value); Map_SetView(); } 
+  		}
+		
+  		public static readonly DependencyProperty longitude_Property = 
+    		DependencyProperty.Register("longitude", typeof(double?), typeof(MapCtrl),
+			new PropertyMetadata(new PropertyChangedCallback(OnChanged_longitude_Property))); 
+		
+		private static void OnChanged_longitude_Property(
+			DependencyObject d, DependencyPropertyChangedEventArgs e) 
+		{
+			//((MapCtrl)d).Map_SetView();
+		}
+		
+		
+		private bool Map_IsSetting = false;
+		public void Map_SetView() 
+		{
+			Map_IsSetting = true;
+			if (!latitude.HasValue) return;
+			if (!longitude.HasValue) return;
+			if (!zoomLevel.HasValue) return;
+			
+			MyMap.SetView(new Location(latitude.Value, longitude.Value), zoomLevel.Value);
+			Map_IsSetting = false;
+		}
+
+		
+		public MapCtrl()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+			if (!latitude.HasValue) latitude = default(Double);
+			if (!longitude.HasValue) longitude = default(Double);
+			if (!zoomLevel.HasValue) zoomLevel = default(Double);
+			
+			MyMap.ViewChangeEnd +=new System.EventHandler<Microsoft.Maps.MapControl.MapEventArgs>(MyMap_ViewChangeEnd);
+			Map_SetView();
+		}
+
+		
+		private void MyMap_ViewChangeEnd(object sender, Microsoft.Maps.MapControl.MapEventArgs e)
+		{
+			if (Map_IsSetting) return;
+			latitude = MyMap.Center.Latitude;
+			longitude = MyMap.Center.Longitude;
+			zoomLevel = MyMap.ZoomLevel;
+		}
+		
+
+		
+	}
+}
